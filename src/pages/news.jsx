a2e500197288448f4a12dc44351a9e0bd29b6ed6
Richard <87782510+mt-richard@users.noreply.github.com>
2023-01-25 15:24:01 +0200

    Update news.jsx

--- src/pages/news.jsx
+++ src/pages/news.jsx
@@ -10,7 +10,7 @@ const News = () =>{
             <div className="bg-gray-100 pb-20 pt-24 md:pt-10 p-5 md:pl-56 md:pr-56">
                 <h1 className="uppercase text-xl md:text-2xl font-bold text-gray-600">Inkuru zatambutse</h1>
                 <p className="text-[14px] md:text-lg font-light">Kurikirana inkuru zatambutse kurubuga rwacu.</p>
-                <div className="pt-5 md:grid grid-cols-3 rounded md:pl-10 md:pr-18">
+                <div className="pt-5 md:grid grid-cols-3 gap-3 rounded md:pl-10 md:pr-18">
                     <New title="Lorem ipsmin" content="Note that the development build is not optimized." photo={pic1} arrow={arrow}/>
                     <New title="Lorem ipsmin" content="Note that the development build is not optimized." photo={pic1} arrow={arrow}/>
                     <New title="Lorem ipsmin" content="Note that the development build is not optimized." photo={pic1} arrow={arrow}/>
