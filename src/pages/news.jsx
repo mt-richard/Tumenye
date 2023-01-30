b7f813108968d6e784303e99063f408bdcce0404
mt-richard <Richard@festimoltd.com>
2023-01-30 16:40:55 +0200

    my day 1

--- src/pages/news.jsx
+++ src/pages/news.jsx
@@ -7,7 +7,7 @@ import arrow from '../images/arrow.svg'
 const News = () =>{
     return(
         <div>
-            <div className="bg-gray-100 pb-20 pt-24 md:pt-10 p-10  md:pl-40 md:pr-40">
+            <div className="bg-gray-100 pb-20 pt-24 md:pt-10 p-5  md:pl-40 md:pr-40">
                 <h1 className="uppercase text-xl md:text-2xl font-bold text-gray-600">Inkuru zatambutse</h1>
                 <p className="text-[14px] md:text-lg font-light">Kurikirana inkuru zatambutse kurubuga rwacu.</p>
                 <div className="pt-5 md:grid grid-cols-3 md:gap-10 rounded md:pl-10 md:pr-18 lg:pl-0 lg:pr-0">
